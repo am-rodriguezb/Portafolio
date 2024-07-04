56488930be5dd17fa0031d6ba0923ee3fbd7f103
am-rodriguezb <134561449+am-rodriguezb@users.noreply.github.com>
2024-07-04 01:18:41 -0400

    entrega
    guia terminada

--- portfolioproject/myportfolio/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Tecnologia, Proyecto
-
-# Register your models here.
-#Registro de modelos en el panel de administración
-admin.site.register(Categoria)
-admin.site.register(Tecnologia)
-admin.site.register(Proyecto)
